c3a1bc9dfccc1cca52a7a08ca2250b5efb28d10f
VariableNotDef <sayedun10@gmail.com>
2025-08-12 14:23:39 -0400

    change name to Jordan and Shane, changed USAID to analyst

--- app/src/Home.py
+++ app/src/Home.py
@@ -44,7 +44,7 @@ st.write('#### HI! As which user would you like to log in?')
 # functionality, we put a button on the screen that the user 
 # can click to MIMIC logging in as that mock user. 
 
-if st.button("Act as John, a Political Strategy Advisor", 
+if st.button("Act as Shane, a Political Strategy Advisor", 
             type = 'primary', 
             use_container_width=True):
     # when user clicks the button, they are now considered authenticated
@@ -59,7 +59,7 @@ if st.button("Act as John, a Political Strategy Advisor",
     logger.info("Logging in as Political Strategy Advisor Persona")
     st.switch_page('pages/00_Pol_Strat_Home.py')
 
-if st.button('Act as Mohammad, an USAID worker', 
+if st.button('Act as Jordan, an Analyst', 
             type = 'primary', 
             use_container_width=True):
     st.session_state['authenticated'] = True
